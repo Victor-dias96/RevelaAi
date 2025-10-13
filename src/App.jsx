4fe4bacd143db71bcd8c9f115bfcda6d3eb94c16
Victor-dias96 <diasv3882@gmail.com>
2025-10-13 19:05:22 +0000

    feat/não-funcional-header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Revela aí</h1>
-      <p>Em construção...</p>
-      <p>Danilo esteve por aqui...</p>
-    </>
+    <div className="aplicacao-container">
+      <Header />
+
+      {}
+      <main className="conteudo-principal">
+        <Home />
+      </main>
+
+      <Footer />
+    </div>
   );
 }
+
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <h1>Revela aí</h1>
+      <p>Em construção...</p>
+      <p>Danilo esteve por aqui...</p>
+    </>
+  );
+}
+
+export default Home;
