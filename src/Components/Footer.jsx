8eace52ff4f136ff09b18c688d0101ec3868f145
Danilo Araujo <danilocostafp@gmail.com>
2026-01-12 15:27:18 -0300

    feat(home): add parties carousel

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,41 @@
+import { useRef } from "react";
 import { logoConverterMap } from "../utils/logoConverter";
+import chevron from "../assets/right-thin-chevron.svg";
 import "./Footer.css";
 
 const Footer = ({ message, partidos }) => {
+  const carrossel = useRef(null);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+
+    carrossel.current.scrollLeft -= carrossel.current.offsetWidth * 0.8;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+
+    carrossel.current.scrollLeft += carrossel.current.offsetWidth * 0.8;
+  };
+
   return (
     <footer className="footer">
       <p>{message}</p>
-      <div className="partidos">
+      <div className="partidos" ref={carrossel}>
         {partidos.map((partido) => {
           const logoUrl =
             logoConverterMap[partido.sigla] || logoConverterMap["DEFAULT"];
 
           return <img key={partido.id} src={logoUrl} alt={partido.nome} />;
         })}
+      </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}>
+          <img src={chevron} alt="Ir para esquerda" />
+        </button>
+        <button onClick={handleRightClick}>
+          <img src={chevron} alt="Ir para direita" />
+        </button>
       </div>
     </footer>
   );
